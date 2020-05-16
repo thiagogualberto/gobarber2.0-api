9dee3e6b90162d7d76417f749f73b85714023f08
Thiago Gualberto <thiago.gou@gmail.com>
2020-05-16 20:42:12 -0300

    password encryption

--- src/services/CreateUserService.ts
+++ src/services/CreateUserService.ts
@@ -1,5 +1,7 @@
 import { getRepository } from 'typeorm';
 
+import { hash } from 'bcryptjs';
+
 import User from '../models/User';
 
 interface Request {
@@ -20,10 +22,12 @@ class CreateUserService {
       throw new Error('Email addres already used.');
     }
 
+    const hashedPassword = await hash(password, 8);
+
     const user = userRepository.create({
       name,
       email,
-      password,
+      password: hashedPassword,
     });
 
     await userRepository.save(user);
