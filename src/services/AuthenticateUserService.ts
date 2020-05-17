8958276be44d7d91d38453ab880af1ae5210a437
Thiago Gualberto <thiago.gou@gmail.com>
2020-05-17 10:50:30 -0300

    Generate JWT token

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,6 +1,6 @@
 import { getRepository } from 'typeorm';
-
 import { compare } from 'bcryptjs';
+import { sign, verify } from 'jsonwebtoken';
 
 import User from '../models/User';
 
@@ -11,6 +11,7 @@ interface Request {
 
 interface Response {
   user: User;
+  token: string;
 }
 
 class AuthenticateUserService {
@@ -31,8 +32,14 @@ class AuthenticateUserService {
       throw new Error('Incorrect email/password combination.');
     }
 
+    const token = sign({}, '5b0ca1bf038c74e2f92fb4ee5247772a', {
+      subject: user.id,
+      expiresIn: '1d',
+    });
+
     return {
       user,
+      token,
     };
   }
 }
